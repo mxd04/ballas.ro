bfc514624d025e524736adbd5f94233f29540df2
mxd04 <alex.mxldo@gmail.com>
2026-05-03 20:30:12 +0300

    modificat titlu si iconita

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,25 +7,25 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Landing Page',
+  title: 'Ballas - Bahamas Club',
   description: 'Bine ai venit pe site-ul nostru',
   generator: 'v0.app',
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/B.jpg',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/B.jpg',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/B.jpg',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/B.jpg',
   },
 }
 
